5f58d36671dcb0dd74a6b8344df294acac70ef55
Daniel Wu <daniel.wu06@sap.com>
2022-05-03 21:44:36 +0800

    test

---
+++ demo/config.js
@@ -0,0 +1,21 @@
+{
+  "version": "1.0",
+  "logger" : {
+    "name" : "git2consul",
+    "streams" : [{
+      "level": "trace",
+      "stream": "process.stdout"
+    }]
+  },
+  "repos" : [{
+    "name" : "sample_configuration",
+    "url" : "https://github.com/DanielYWoo/git2consul.git",
+    "source_root": "/demo/data",
+    "mountpoint": "/git2consul",
+    "branches" : ["master"],
+    "hooks": [{
+      "type" : "polling",
+      "interval" : "1"
+    }]
+  }]
+}
---
+++ demo/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../node_modules/.bin/git2consul --config-file config.js
